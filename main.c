a3c852d8b32f1490b1c395d0bd74dbf870af22b0
TheFatLady <48660800+TheFatLady@users.noreply.github.com>
2019-04-04 15:31:03 +1100

    Create main.c
    initial cypher stuff

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int a(int bef[3]={"raw"}, int aft[3]={"enc"});
+
+int main (void)
+{
+    char menu_option;
+    
+
+    printf("    ROTATION AND SUBSTITUTION CYPHER ENCRYPTION/DECRYPTION\n");
+    printf("---------------------------------------------------------------\n\n");
+
+
+    do{
+    printf("Main Menu\n");
+    printf("a. ENCRYPT with rotation cypher\n");
+    printf("b. DECRYPTION of rotation cypher given rotation amount\n");
+    printf("c. DECRYPTION of rotation cypher\n");
+    printf(" Please enter an option from the main menu: ");
+    scanf("%c",&menu_option);
+
+
+
+    switch(menu_option){
+
+    case 'a':
+        printf("Searching for data...");
+        printf("encrypting...");
+        a(aft[n]);
+        for (n = 0, n<25, n++){
+        printf(" %d", aft[n]);
+        break;
+       return 0;
+       
+    default:
+        printf("invalid input");
+            break;
+    }
+
+    }while(menu_option !='e');
+    }
+    return 0;
+}
+
+int a(int bef[7]={"message"}){ 
+    char enc, key = 4;
+    int i = 0, n = 0;
+    while (i<10){   //make it less than letters  
+    aft[n] = (bef[i]+key)*(%26);
+    i++;
+    }
+}
